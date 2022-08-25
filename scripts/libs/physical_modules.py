2ba9fa6d870b04090a5ebff175e1a87c7e320649
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-08-25 14:00:23 -0300

    refactor: more readable module setup

--- scripts/libs/physical_modules.py
+++ scripts/libs/physical_modules.py
@@ -1,13 +1,17 @@
-import machine
 import dht
+from machine import Pin, ADC, Signal
 from libs.constants import DHT_SENSOR_PIN, INTERNAL_LED_PIN, LDR_ADC
 
 
 def setup():
-    internal_led = machine.Signal(INTERNAL_LED_PIN, machine.Pin.OUT, invert=True)
-    dth_sensor = dht.DHT11(machine.Pin(DHT_SENSOR_PIN))
-    ldr_sensor = machine.ADC(LDR_ADC)
-    physical_modules = {"internal_led": internal_led, "dth_sensor": dth_sensor, "ldr_sensor": ldr_sensor}
+    internal_led = Signal(INTERNAL_LED_PIN, Pin.OUT, invert=True)
+    dth_sensor = dht.DHT11(Pin(DHT_SENSOR_PIN))
+    ldr_sensor = ADC(LDR_ADC)
+    physical_modules = {
+        "internal_led": internal_led,
+        "dth_sensor": dth_sensor,
+        "ldr_sensor": ldr_sensor
+    }
     __set_initial_state(physical_modules)
     return physical_modules
 
