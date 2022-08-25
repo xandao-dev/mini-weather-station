2904e59747430852ed7778912da78091deaeabe0
Alexandre Calil <alexandrecalilmf@gmail.com>
2022-08-25 13:00:29 -0300

    feat: add internal led and ldr to modules

--- scripts/libs/constants.py
+++ scripts/libs/constants.py
@@ -1,3 +1,5 @@
 from micropython import const
 
 DHT_SENSOR_PIN = const(4)  # Pin 4 is D2 on Wemos D1 Mini
+INTERNAL_LED_PIN = const(2)  # Pin 2 is D4 on Wemos D1 Mini
+LDR_ADC = const(0)  # ADC 0 is A0 on Wemos D1 Mini
--- scripts/libs/physical_modules.py
+++ scripts/libs/physical_modules.py
@@ -1,9 +1,17 @@
 import machine
 import dht
-from libs.constants import DHT_SENSOR_PIN
+from libs.constants import DHT_SENSOR_PIN, INTERNAL_LED_PIN, LDR_ADC
 
 
 def setup():
+    internal_led = machine.Signal(INTERNAL_LED_PIN, machine.Pin.OUT, invert=True)
     dth_sensor = dht.DHT11(machine.Pin(DHT_SENSOR_PIN))
-    physical_modules = {"dthSensor": dth_sensor}
+    ldr_sensor = machine.ADC(LDR_ADC)
+    physical_modules = {"internal_led": internal_led, "dth_sensor": dth_sensor, "ldr_sensor": ldr_sensor}
+    __set_initial_state(physical_modules)
     return physical_modules
+
+
+def __set_initial_state(physical_modules):
+    internal_led = physical_modules["internal_led"]
+    internal_led.off()
